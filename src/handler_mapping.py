b9d0c112ae23c657541212bad96d35686a3cca03
cylonbrain <cylonbrain@gmail.com>
2014-09-30 18:30:32 +0200

    Update handler_mapping.py

--- src/handler_mapping.py
+++ src/handler_mapping.py
@@ -3,10 +3,6 @@ Created on 4 Oct 2011
 
 @author: Steven
 '''
-from rss_source import mitbbs
-from rss_source import cnbeta
-from rss_source import powerapple
+from rss_source import heise
 
-handlers = {"mitbbs": (mitbbs.handler,"http://www.mitbbs.com/rss/top.xml"),
-            "cnbeta": (cnbeta.handler,"http://www.cnbeta.com/backend.php"),
-			"powerapple": (powerapple.handler,"http://bbs.powerapple.com/forum.php?mod=rss&auth=0")}
+handlers = {"heise": (mitbbs.handler,"http://www.heise.de/newsticker/heise-atom.xml")}
